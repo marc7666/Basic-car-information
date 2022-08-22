bea44b247e5f969029690d4623218dc3bd1fb9db
marc7666 <marc7666@github.com>
2022-08-22 12:49:37 +0200

    data written in file

--- car_info.py
+++ car_info.py
@@ -10,6 +10,8 @@ from Exceptions.TractionException import TractionException
 from Exceptions.TransmissionException import TransmissionException
 from Exceptions.VINException import VINException
 from Exceptions.VehicleTypeNotValidException import VehicleTypeNotValidException
+from colorama import Fore
+import time
 
 
 # pylint: disable=C0103, R0902, R0903, R0913
@@ -43,6 +45,10 @@ class CarInfo:
         self.tyre_speed_rating = tyre_speed_rating
 
     def check_type(self):
+        """
+        This function checks if the car type is electric, hybrid or combustion
+        :return: True or raises an Exception
+        """
         if (self.vehicle_type == "Hybrid" or self.vehicle_type == "hybrid" or
                 self.vehicle_type == "electric" or self.vehicle_type == "Electric" or
                 self.vehicle_type == "combustion" or self.vehicle_type == "Combustion"):
@@ -51,12 +57,20 @@ class CarInfo:
             raise VehicleTypeNotValidException("Vehicle type not valid")
 
     def check_vin(self):
+        """
+        Checks if the VIN has the appropriate length
+        :return: True or raises and exception
+        """
         if len(self.vin) == 17:
             return True
         else:
             raise VINException("VIN format error")
 
     def check_distinction(self):
+        """
+        Checks the environmental distinction type
+        :return: True or raises and exception
+        """
         if (self.environmental_distinction == "C" or self.environmental_distinction == "B" or
                 self.environmental_distinction == "ECO" or self.environmental_distinction == "0"):
             return True
@@ -64,12 +78,22 @@ class CarInfo:
             raise EnvironmentalDistinctionException("Environmental distinction not valid")
 
     def check_km(self):
+        """
+        Checks if the number of km of the car is in the range 0, 999999
+        :return: True or raises and exception
+        """
         if self.km < 0:
             raise KmException("Too few kilometers")
         elif self.km > 999999:
             raise KmException("Too much kilometers")
+        else:
+            return True
 
     def check_fuel(self):
+        """
+        Checks the fuel type
+        :return: True or raises and exception
+        """
         if self.vehicle_type == "Hybrid" or self.vehicle_type == "hybrid":
             if self.fuel == "Gasoline/Electric" or self.fuel == "gasoline/electric":
                 return True
@@ -87,6 +111,10 @@ class CarInfo:
                 raise FuelException("Fuel type not valid")
 
     def check_transmission(self):
+        """
+        Checks the transmission type
+        :return: True or raises and exception
+        """
         if (self.propulsion_transmission == "Automatic" or self.propulsion_transmission == "automatic"
                 or self.propulsion_transmission == "Manual" or self.propulsion_transmission == "manual" or
                 self.propulsion_transmission == "Sequential" or self.propulsion_transmission == "sequential"):
@@ -95,6 +123,10 @@ class CarInfo:
             raise TransmissionException("Propulsion transmission type not valid")
 
     def check_traction(self):
+        """
+        Checks the traction type
+        :return: True or raises and exception
+        """
         if (self.traction == "AWD" or self.traction == "RWD"
                 or self.traction == "FWD" or self.traction == "4AWD" or self.traction == "4"):
             return True
@@ -102,13 +134,84 @@ class CarInfo:
             raise TractionException("Traction type not valid")
 
     def check_speed_rating(self):
+        """
+        Checks the seed rating letter
+        :return: True or raises and exception
+        """
         if ("L" <= self.tyre_speed_rating <= "Y"
-        or self.tyre_speed_rating == "(Y)"):
+                or self.tyre_speed_rating == "(Y)"):
             return True
         else:
             raise SpeedRatingException("Speed rating exception not valid")
 
     def write_in_file(self):
-        if(self.check_type() and self.check_vin() and self.check_distinction()
-        and self.check_km() and self.check_fuel() and self.check_transmission()
-        and self.check_traction() and self.check_speed_rating()):
+        """
+        Checks all the parameters before writing it in a txt file
+        :return: void method
+        """
+        print(Fore.CYAN + "Checking the introduced parameters, please wait...")
+        if (self.check_type() and self.check_vin() and self.check_distinction()
+                and self.check_km() and self.check_fuel() and self.check_transmission()
+                and self.check_traction() and self.check_speed_rating()):
+            print(Fore.GREEN + "Vehicle type -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Car brand -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Car model -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Car VIN -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Country -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Registration plate -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Car environmental distinction -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Car km -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Car fuel type -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Engine displacement -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Car power -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Car transmission type -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Car traction site -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Tyre width -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Tyre profile -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Tyre rim size -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Tyre load index -> OK")
+            time.sleep(2)
+            print(Fore.GREEN + "Tyre speed rating -> OK")
+            print(Fore.YELLOW + "All parameters OK, writing in file, please wait...")
+            time.sleep(3)
+            file = open("Car_information.txt", "w")
+            file.write("Vehicle type -> " + self.vehicle_type + "\n")
+            file.write("Vehicle brand -> " + self.brand + "\n")
+            file.write("Vehicle model -> " + self.model + "\n")
+            file.write("Vehicle VIN -> " + self.vin + "\n")
+            file.write("Registration country -> " + self.country + "\n")
+            file.write("Registration plate -> " + self.registration_plate + "\n")
+            file.write("Environmental distinction -> " + self.environmental_distinction + "\n")
+            file.write("Kilometers -> " + str(self.km) + "\n")
+            file.write("Vehicle fuel -> " + self.fuel + "\n")
+            file.write("Engine displacement -> " + str(self.engine_displacement) + "\n")
+            file.write("Vehicle power -> " + str(self.power) + "\n")
+            file.write("Vehicle transmission -> " + self.propulsion_transmission + "\n")
+            file.write("Vehicle traction -> " + self.traction + "\n")
+            file.write("Tyre measures -> " + str(self.tyre_width) + "/" + str(self.tyre_profile) +
+                       " R " + str(self.tyre_rim_size) + " " + str(self.tyre_load_index) + self.tyre_speed_rating)
+            file.close()
+            print(Fore.MAGENTA + "All information written in \"Car_information.txt\" file")
+
+
+if __name__ == "__main__":
+    car = CarInfo("Hybrid", "Toyota", "Yaris Cross", "4788 MMM", "Spain", "12345678945217985", "ECO",
+                  2000, "Gasoline/Electric", 115, 1500, "Automatic",
+                  "FWD", 215, 55, 17, 94, "V")
+    car.write_in_file()
